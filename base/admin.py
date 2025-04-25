ff0e49746b307a20def7b302a4daf8a85056776d
zinchuriki <priyanshu999.ps@gmail.com>
2025-04-25 16:29:59 +0530

    initial commit

---
+++ base/admin.py
@@ -0,0 +1,10 @@
+from email import message
+from django.contrib import admin
+
+# Register your models here.
+
+from.models import Room,Message,Topic,User
+admin.site.register(Room)
+admin.site.register(Message)
+admin.site.register(Topic)
+admin.site.register(User)
