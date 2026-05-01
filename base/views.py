b80aab011fc4c6d3ddafb2f975b9d03fb728a88c
Priyanshu Sharma <priyanshu999.ps@gmail.com>
2026-05-01 23:59:14 +0530

    Fix indentation in activityPage function for consistency

--- base/views.py
+++ base/views.py
@@ -211,7 +211,8 @@ def topicsPage(request):
     return render(request, "base/topics.html", context)
 
 
-def activityPage(request):
+ def activityPage(request):
     room_messages = Message.objects.all()
     context = {"room_messages": room_messages}
     return render(request, "base/activity.html", context)
+
