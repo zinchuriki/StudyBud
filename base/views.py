dc26a4fda4b3965915756e39c9db6dcec456d3e6
Priyanshu Sharma <priyanshu999.ps@gmail.com>
2026-05-02 21:25:43 +0530

    Add whitespace for improved readability before activityPage function

--- base/views.py
+++ base/views.py
@@ -211,6 +211,8 @@ def topicsPage(request):
     return render(request, "base/topics.html", context)
 
 
+ 
+ 
  def activityPage(request):
     room_messages = Message.objects.all()
     context = {"room_messages": room_messages}
